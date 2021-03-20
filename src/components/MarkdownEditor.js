2c74ab1898fa9c074d89067905d6d4119aea9abc
Matej <matejchalachan@gmail.com>
2021-03-20 21:02:43 +0100

    closes #4

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 import ReactMarkdown from 'react-markdown'
 
 const MarkdownEditor = () => {
 
+    const textAreaRef = useRef(null);
     const [markdown, setMarkdown] = useState('# supp');
+    const [textAreaHeight, setTextAreaHeight] = useState("auto");
+    const [parentHeight, setParentHeight] = useState("auto");
+
+    useEffect(() => {
+        setParentHeight(`${textAreaRef.current.scrollHeight}px`);
+        setTextAreaHeight(`${textAreaRef.current.scrollHeight}px`);
+    }, [markdown]);
 
     function handleChange(e) {
+        setTextAreaHeight("auto");
+        setParentHeight(`${textAreaRef.current.scrollHeight}px`);
         setMarkdown(e.target.value)
     }
 
     return (
         <div className="markdown w-full h-full bg-green-300 mx-auto flex justify-center">
             <div className="w-1/2 flex flex-col lg:flex-row items-center justify-center space-y-2 lg:space-y-0 lg:space-x-2">
-                <div className="h-3/4 p-4 w-1/2 bg-white rounded border border-gray-100">
-                    <textarea onChange={handleChange } className="w-full h-full" value={markdown} />
+                <div className="p-2 w-1/2 bg-white rounded border border-gray-100" style={{minHeight: parentHeight}}>
+                    <textarea onChange={handleChange } className="w-full" value={markdown} ref={textAreaRef} rows={1} style={{height: textAreaHeight}}/>
                 </div>
 
                 <ReactMarkdown className="h-3/4 p-4 w-1/2 bg-gray-100 rounded border border-gray-200 overflow-auto" source={markdown}/>
