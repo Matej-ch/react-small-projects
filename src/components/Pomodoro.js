0f53f1b07ca71ca66a32fb73435b685282e31872
Matej <matejchalachan@gmail.com>
2021-07-15 21:17:43 +0200

    double click for pomodoro and input switch

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -8,6 +8,7 @@ const Pomodoro = () => {
     const [timeLeft ,setTimeLeft] = useState(25*60);
     const [isRunning, setIsRunning] = useState(false);
     const [progress,setProgress] = useState(0);
+    const [isEditing, setIsEditing] = useState(false);
 
     const minutes = padTime(Math.floor(timeLeft / 60));
     const seconds = padTime(timeLeft - minutes * 60);
@@ -52,13 +53,23 @@ const Pomodoro = () => {
         setIsRunning(false);
     }
 
+    function markAsEditing() {
+        setIsEditing(editing => {
+            return !editing;
+        });
+    }
+
     return (
         <div className="pomodoro flex flex-col justify-center w-full h-full items-center space-y-2 bg-blue-300">
             <h2 className="font-semibold uppercase text-xl lg:text-3xl text-blue-900 text-opacity-80">{title}</h2>
 
-            <div style={{ width: 200, height: 200 }}>
+            {!isEditing ? (<div style={{ width: 200, height: 200 }} onDoubleClick={() => markAsEditing()}>
                 <CircularProgressbar minValue={0} maxValue={25*60} value={progress} text={`${minutes}:${seconds}`} strokeWidth={3} />
-            </div>
+            </div>) : (<div style={{ width: 200, height: 200 }}  onDoubleClick={() => markAsEditing()}>
+                <input type="number" step="1" min="1" placeholder="minutes" defaultValue={25}/>
+                <input type="number" step="1" min={0} max={60} defaultValue={0}/>
+            </div>)
+            }
 
             <div className="buttons space-y-2 lg:space-x-2 w-full flex flex-col lg:flex-row justify-center items-center lg:items-baseline">
 
