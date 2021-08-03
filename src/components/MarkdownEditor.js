ae32225dcea4f9eee5fdbe8f52a13a4b40128324
Matej <matejchalachan@gmail.com>
2021-08-03 19:37:16 +0200

    markdown editor background

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -20,7 +20,7 @@ const MarkdownEditor = () => {
     }
 
     return (
-        <div className="markdown w-full h-full bg-green-300 mx-auto flex justify-center">
+        <div className="markdown w-full h-full mx-auto flex justify-center markdown-bc">
             <div className="w-1/2 flex flex-col lg:flex-row items-center justify-center space-y-2 lg:space-y-0 lg:space-x-2">
                 <div className="p-2 w-1/2 bg-white rounded border border-gray-100" style={{minHeight: parentHeight}}>
                     <textarea onChange={handleChange } className="w-full" value={markdown} ref={textAreaRef} rows={1} style={{height: textAreaHeight}}/>
