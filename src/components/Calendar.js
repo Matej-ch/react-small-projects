31da6c830ac8032e81fe9bdd5d820bf11d8cc913
Matej <matejchalachan@gmail.com>
2021-10-11 20:39:46 +0200

    updating calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const today = new Date();
 const currentMonth = today.getMonth();
@@ -12,6 +12,31 @@ const Calendar = () => {
     const [choosingType, setChoosingType] = useState('start');
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
+
+    const [month,setMonth] = useState(0);
+    const [day,setDay] = useState(0);
+    const [year,setYear] = useState(0);
+
+    useEffect(() => {
+        const date = new Date();
+
+        setYear(date.getFullYear());
+        setMonth(date.getMonth());
+        setDay(date.getDate());
+
+    },[]);
+
+    const days = ['Sun',
+        'Mon',
+        'Tue',
+        'Wed',
+        'Thu',
+        'Fri',
+        'Sat'];
+
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    ];
 
     const calendarDates = Array(daysInMonth(currentMonth,currentYear)).fill(0).map((e,i) => i);
 
@@ -35,6 +60,39 @@ const Calendar = () => {
         if(choosingType === 'end') {
             setEndDate(chosenDay);
         }
+    }
+
+    function nextMonth()
+    {
+        console.log(month);
+        if ( month === 11 ) {
+            setMonth(0);
+            setYear(year => year + 1);
+        }
+        else {
+            setMonth(month => month + 1);
+        }
+        currMonth();
+    }
+
+    function previousMonth()
+    {
+        if ( month === 0 ) {
+            setMonth(11);
+            setYear(year => year - 1);
+        }
+        else {
+            setMonth(month => month - 1)
+        }
+        currMonth();
+    }
+
+    function currMonth()
+    {
+
+    }
+
+    function daysOnScreen() {
 
     }
 
@@ -42,7 +100,14 @@ const Calendar = () => {
         <div className="flex flex-col justify-center w-full h-full items-center space-y-2 bg-gradient-to-b from-red-200 to-red-300">
 
             <div className="calendar">
-                <div className="date-chooser flex flex-row justify-between w-full max-w-sm">
+                
+                <div className="date-chooser flex flex-row justify-between w-full max-w-sm flex-wrap">
+
+                    <div className={'flex flex-row justify-between w-full pb-4'}>
+                        <button className={'font-bold text-white p-2 border rounded-sm hover:bg-gray-700'} onClick={previousMonth}>Previous</button>
+                        <button className={'font-bold text-white p-2 border rounded-sm hover:bg-gray-700'} onClick={nextMonth}>Next</button>
+                    </div>
+
                     <button className="date-chooser-button flex flex-col" onClick={() => setChoosingType('start')}>
                         Start date <span>{startDate}</span>
                     </button>
@@ -52,6 +117,14 @@ const Calendar = () => {
                 </div>
 
                 <div className="flex flex-row flex-wrap max-w-sm font-bold justify-start">
+                    <div className={'flex flex-col justify-center w-full'}>
+                        <div className={'text-center'}>{ months[month] } { year }</div>
+                        <div className={'flex justify-between px-4 mt-2'}>{ days.map((day,index) => {
+                            return <div>{day}</div>
+                        }) }</div>
+
+                    </div>
+
                     <div>
                         {calendarDates.map((day,index) => {
                             const dayNum = day + 1;
